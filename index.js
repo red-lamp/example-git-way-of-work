60115563dad0f234c3192d3d5253762e73e21443
Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
2019-09-23 16:49:16 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ ############################################
+ * @author Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
+ * Use as a main function for testing create pull request to git flow
+ ############################################
+ */
+
+function main() {
+    console.log('this is a main class, master branch')
+}
+
+main()
