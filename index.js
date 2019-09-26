7e768288fa0234f0a3eadd2531267bcfa66a2933
Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
2019-09-26 16:12:00 +0700

    change to test

--- index.js
+++ index.js
@@ -6,19 +6,19 @@
  */
 
 function main() {
-    console.log('this is a main class, dev branch')
+    console.log('this is a main class, test branch')
 }
 
 function feature1() {
-    console.log('this is a main class, dev branch', 'feature1')
+    console.log('this is a main class, test branch', 'feature1')
 }
 
 function feature2() {
-    console.log('this is a main class, dev branch', 'feature2')
+    console.log('this is a main class, test branch', 'feature2')
 }
 
 function feature3() {
-    console.log('this is a main class, dev branch', 'feature3')
+    console.log('this is a main class, test branch', 'feature3')
 }
 
 main()
