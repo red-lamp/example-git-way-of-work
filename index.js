5ceb1adfda0c7c5f9618ffa634243329c17d3106
Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
2019-09-27 16:44:40 +0700

    add feature8

--- index.js
+++ index.js
@@ -37,4 +37,8 @@ function feature7() {
     console.log('this is a main class, dev branch', 'feature7')
 }
 
+function feature8() {
+    console.log('this is a main class, dev branch', 'feature8')
+}
+
 main()
