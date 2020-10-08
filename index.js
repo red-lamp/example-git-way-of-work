7f58a3e69cf2f1f5e420536933457411d0d345a6
Amarit Jarasjindarat <amarit.jarasjindarat@gmail.com>
2020-10-08 16:16:38 +0700

    add feature23

--- index.js
+++ index.js
@@ -97,4 +97,8 @@ function feature21() {
     console.log('this is a main class', 'feature21_fixed_bugs')
 }
 
+function feature23() {
+    console.log('this is a main class', 'feature23')
+}
+
 main()
